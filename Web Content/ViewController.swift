bcbb5a49aba0a52b19ea447fa3f57579a6054b88
Gerardo Camilo <gerardo.camilo@gmail.com>
2014-11-03 19:46:44 -0400

    Initial Commit

---
+++ Web Content/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web Content
+//
+//  Created by Gerardo Camilo on 03/11/14.
+//  Copyright (c) 2014 ___GRCS___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
